29e5beefdb8fd719fdb011d56f0b6b543f83c6e2
NimeshaMihiranga <nmacademy.lk@gmail.com>
2026-06-17 04:54:36 +0530

    finish sidebar

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.querySelector('.menu-btn').addEventListener('click' , function()
-{
-    Document.querySelector('.sidebar').classList.toggle('active');
+document.getElementById('menu-btn').addEventListener('click', function() {
+    const icon = this.querySelector('i');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
+});
+
+const menuBtn = document.getElementById('menu-btn');
+const sidebar = document.querySelector('.sidebar');
+const icon = menuBtn.querySelector('i');
+
+menuBtn.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+    
+    if (sidebar.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 });
